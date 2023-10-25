e8a3a8441e5699676306096e0a7dafc6f1749f98
dev-rsiva <dev.rsiva@gmail.com>
2023-10-25 19:54:01 +0530

    chapter 02 is completed

--- Chapter 02- Igniting Our App/Codes/App.js
+++ Chapter 02- Igniting Our App/Codes/App.js
@@ -1,3 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", {}, "This is h1"),
