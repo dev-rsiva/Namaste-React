ac278936ff49d3f0c39325a368ea27f43adb12ea
dev-rsiva <dev.rsiva@gmail.com>
2023-10-25 13:43:34 +0530

    first commit

---
+++ Chapter 02- Igniting Our App/Codes/App.js
@@ -0,0 +1,17 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is h1"),
+    React.createElement("h2", {}, "This is h2"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is h1"),
+    React.createElement("h2", {}, "This is h2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+console.log(parent)
